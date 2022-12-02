d92018faa48d170aff75b245f0dcd76ac7fecc93
mtbell412 <mtbell412@gmail.com>
2022-12-01 22:01:57 -0600

    initial commit with some models done

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const dayjs = require('dayjs');
+const {Schema, model} = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+    thoughtText:{
+        type: String,
+        required: true,
+        max_length: 280,
+        min_length: 1
+    },
+    createAt:{
+        type: Date,
+        default: Date.now(),
+        get: (time) => {
+            var x = dayjs(time).format('mm/dd/yyyy')
+            return x;
+        }
+    },
+    username:{
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema]
+    
+},
+{
+    toJSON:{
+        virtuals: true,
+        getters: true
+    },
+    id: false,
+}
+
+)
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length
+})
+
+const Thought = model('thought',thoughtSchema);
+
+module.exports = Thought;
